dabf4920fb0db31bd73678d1be778161f6fe9b45
Luis Gabriel <luisgabriel19396@gmail.com>
2026-08-14 15:23:50 -0300

    feat: testes(atualiza projeto)

--- app/main.py
+++ app/main.py
@@ -3,29 +3,29 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def home ():
-    return {
-        "Mensagem": "API FastAPI",
-        }
+def raiz():
+    return { "mensagem" : "API FastAPI Funcionando." }
+
 
 @app.get("/health")
 def health():
-    return {
-        "Status": "online"
-    }
+    return { "status": "ok" }
+
 
 @app.get("/soma")
 def soma(a: int, b: int):
-    return {"Resultado": a + b}
-    
+    return {"resultado": a + b}
+
 from pydantic import BaseModel
+
 class Tarefa(BaseModel):
     titulo: str
     concluida: bool = False
+
 
 @app.post("/tarefas")
 def criar_tarefa(tarefa : Tarefa):
     return {
         "mensagem": "Tarefa recebida com sucesso",
-        "dados": tarefa
+        "dados" : tarefa
     }
